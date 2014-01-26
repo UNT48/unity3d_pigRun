54551857e5d1279e83a9ad435548830ea9cab3a9
UNT48 <wistariai117@gmail.com>
2014-01-26 20:15:03 +0900

    タイトル画面・ルール画面作成

---
+++ Assets/Scripts/CTitle.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CTitle : MonoBehaviour {
+
+	public Texture ButtonImage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		if(Input.GetButton("Jump")) {
+//			Application.LoadLevel("Rules");
+//		}
+
+//		// click start
+//		if(Input.GetButton("Jump")) {
+//			Application.LoadLevel("Main");
+//		}
+
+//		// click rules
+//		if(Input.GetMouseButtonDown(0)) {
+//			Application.LoadLevel("Rules");
+//		}
+
+	}
+
+	// GUI描画
+	void OnGUI() {
+
+		Rect rect_rules = new Rect(100, 230, 150, 100);
+		bool isClicked_rules = GUI.Button(rect_rules, ButtonImage, "fixedWidth");
+
+//		bool isClicked_rules = GUI.Button(rect_rules, new GUIContent("Push!!", ButtonImage), "fixedWidth");
+
+
+		// click rules button
+		if (isClicked_rules){
+			// jump rules
+			Application.LoadLevel("Rules");
+
+		}
+
+		Rect rect_start = new Rect(350, 230, 150, 100);
+		bool isClicked_start = GUI.Button(rect_start, ButtonImage, "fixedWidth");
+
+		// click start button
+		if (isClicked_start){
+		// jump main
+			Application.LoadLevel("Main");
+		}
+
+	}
+
+
+}
---
+++ Assets/Scripts/Rules.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rules : MonoBehaviour {
+
+	public Texture ButtonImage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		if(Input.GetButton("Jump")) {
+//			Application.LoadLevel("Title");
+//		}
+		
+		// click rules
+//		if(Input.GetMouseButtonDown(0)) {
+//			Application.LoadLevel("Title");
+//		}
+
+	}
+
+//	var btnTexture : Texture;
+
+	void OnGUI(){
+
+//		if (!btnTexture) {
+//			Debug.LogError("Please assign a texture on the inspector");
+//			return;
+//		}
+
+		Rect rect = new Rect(520, 0, 80, 80);
+
+//		bool isClicked = GUI.Button(rect, new GUIContent("Push!!", ButtonImage));
+		bool isClicked = GUI.Button(rect, ButtonImage, "fixedWidth");
+
+		if (isClicked){
+			// jump title
+			Application.LoadLevel("Title");			
+		}
+	}
+
+}
