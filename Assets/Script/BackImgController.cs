bdf31dc5d63d0af72e5d4dd74a09adf52f3de60b
to-miyake <rascal.miyake@gmail.com>
2014-01-26 19:39:26 +0900

    最新版をマージ

--- Assets/Script/BackImgController.cs
+++ Assets/Script/BackImgController.cs
@@ -2,21 +2,10 @@
 using System.Collections;
 
 public class BackImgController : MonoBehaviour {
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	
+
 	void OnBecameInvisible () {
 		Vector3 floor_position = this.transform.position;
-		//floor_position.x += this.transform.localScale.x;
-		floor_position.x += 155;
+		floor_position.x += 232.5f;
 		this.transform.position = floor_position;
 	}
 }
--- Assets/Script/GroundController.cs
+++ Assets/Script/GroundController.cs
@@ -5,15 +5,23 @@ public class GroundController : MonoBehaviour {
 
 	public GameObject[] ground;
 
-	private int _moveScale = 155;
+	private float _moveScale = 232.5f;
 
 	void OnBecameInvisible () {
-	
+		int randNum = 0;
 		Vector3 floor_position = this.transform.position;
-		floor_position.x += _moveScale;
 
-		Instantiate (this.ground[Random.Range(0, ground.Length)], floor_position, this.transform.rotation);
+		floor_position.x += _moveScale;
+		randNum = Random.Range(0, ground.Length);
+
+		Instantiate (ground[randNum], floor_position, this.transform.rotation);
 
 		Destroy (this.gameObject);
+	}
+
+	void OnApplicationQuit ()
+	{
+		Debug.Log (this.gameObject.name + " OnApplicationQuit");
+		Destroy (gameObject);
 	}
 }
