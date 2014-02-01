962cee2bc5ed92bf390351d094541487268f3922
UNT48 <wistariai117@gmail.com>
2014-02-01 16:03:03 +0900

    タイトル画面・ルール画面追加Revenge

---
+++ Assets/Script/CTitle.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CTitle : MonoBehaviour {
+
+	public Texture ButtonImageRules;
+	public Texture ButtonImageLevel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// GUI描画
+	void OnGUI() {
+
+		Rect rect_rules = new Rect(140, 300, 150, 100);
+		bool isClicked_rules = GUI.Button(rect_rules, ButtonImageRules, "fixedWidth");
+
+		// click rules button
+		if (isClicked_rules){
+			// jump rules
+			Application.LoadLevel("Rules");
+
+		}
+
+		Rect rect_start = new Rect(390, 300, 150, 100);
+		bool isClicked_start = GUI.Button(rect_start, ButtonImageLevel, "fixedWidth");
+
+		// click start button
+		if (isClicked_start){
+		// jump main
+			Application.LoadLevel("Level");
+		}
+
+	}
+
+
+}
---
+++ Assets/Script/Rules.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rules : MonoBehaviour {
+
+	public Texture ButtonImage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI(){
+
+		Rect rect = new Rect(570, 0, 80, 80);
+		bool isClicked = GUI.Button(rect, ButtonImage, "fixedWidth");
+
+		if (isClicked){
+			// jump title
+			Application.LoadLevel("Title");			
+		}
+	}
+
+}
